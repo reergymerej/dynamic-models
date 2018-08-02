9b45616935beca1dc19f80f3c605d24276638060
Jeremy Greer <jex.grizzle@gmail.com>
2018-08-02 13:24:31 -0400

    Add queries to add instance values

--- README.md
+++ README.md
@@ -3,3 +3,9 @@ use them.  Imagine an inventory system.  You could provide a way for users to
 define new items so you don't have to anticipate everything up front.
 
 
+TODO
+
+Handle instances.  We've got models, but how do we store instances with their
+instance values?
+
+Does this handle arrays already (multiple image urls for example)?
--- queries.sql
+++ queries.sql
@@ -2,17 +2,16 @@
 --- Define a model, one shot!
 WITH new_model as (
 	-- model
-	insert into model (name) values ('sticker') returning *
+	insert into model (name) values ('shoe') returning *
 ),
 new_attributes as (
 	-- attributes
 	insert into attributes (type, name) values
+		('string', 'brand'),
 		('string', 'description'),
-		('string', 'stock id'),
 		('string', 'image url'),
-		('num', 'on hand'),
 		('num', 'price'),
-		('bool', 'awesome')
+		('num', 'in stock')
 	on conflict on constraint attributes_type_name_key
 	do update set name=EXCLUDED.name
 	returning *
@@ -31,7 +30,7 @@ select m.name, a.type, a.name
 from model m
 left join model_attributes ma on ma.model_id = m.id
 left join attributes a on a.id = ma.attributes_id
-where m.name = 'sticker'
+--where m.name = 'shoe'
 order by m.id
 ;
 
@@ -55,4 +54,79 @@ insert into model_attributes (model_id, attributes_id)
     select m.id from model m where m.name = 'sticker'
   ), na.id from new_attributes na;
 
+
+
+
+----------------------------------
+-- Add instance of model.
+INSERT INTO model_instances (model_id)
+	SELECT m.id FROM model m WHERE name = 'shoe'
+;
+
+-- model instance id
+select mi.id
+from model_instances mi
+where mi.model_id = (
+	SELECT m.id FROM model m WHERE name = 'shoe'
+);
+
+-- Add instance values.
+--	brand: Nike
+--	image url: http://foo.com/1
+--	image url: http://foo.com/2
+--	price: 9.99
+
+select * from attributes_values_string avs;
+
+INSERT INTO attributes_values_string (model_instance_id, attributes_id, value)
+VALUES (
+	2,  -- get from query
+	(
+		select a.id
+		from model m
+		left join model_attributes ma on ma.model_id = m.id
+		left join attributes a on a.id = ma.attributes_id
+		where m.name = 'shoe'
+		and a.name = 'brand'
+	),
+
+	'Adidas'
+);
+
+INSERT INTO attributes_values_num (model_instance_id, attributes_id, value)
+VALUES (
+	2,  -- get from query
+	(
+		select a.id
+		from model m
+		left join model_attributes ma on ma.model_id = m.id
+		left join attributes a on a.id = ma.attributes_id
+		where m.name = 'shoe'
+		and a.name = 'price'
+	),
+
+	19.99
+);
+
+
+
+
+----------------------------------
+-- Get all data for an instance.
+select a.name, avs.value, null as "value"
+from model_instances mi
+join model m on m.id = mi.model_id
+join attributes_values_string avs on avs.model_instance_id = mi.id
+join attributes a on a.id = avs.attributes_id
+where mi.id = 2
+
+UNION ALL
+
+select a.name, null as "value", avn.value
+from model_instances mi
+join model m on m.id = mi.model_id
+join attributes_values_num avn on avn.model_instance_id = mi.id
+join attributes a on a.id = avn.attributes_id
+where mi.id = 2
+;
 
